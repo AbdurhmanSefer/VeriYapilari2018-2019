1921bbcde297aca17cc453af47ea76b3e0d2bc01
AbdurhmanSefer <46073688+AbdurhmanSefer@users.noreply.github.com>
2018-12-22 21:32:46 +0300

    Add files via upload

---
+++ -palindrom-olup-olmadi-ini-master/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tower
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Stack dizi = new Stack();
+            Console.WriteLine("İfade giriniz");
+
+            string ifade = Console.ReadLine();
+
+            for(int i=0;i<ifade.Length;i++)
+            {
+                dizi.Push(ifade[i]);
+            }
+            bool durum = true;
+
+            for (int i = 0; i < ifade.Length; i++)
+            {
+                var  temp = dizi.Pop();
+               if(ifade[i].ToString()!=temp.ToString())
+                {
+                    durum = false;
+                }
+            }
+
+
+            Console.WriteLine(durum);
+            Console.ReadLine();
+        }
+    }
+}
