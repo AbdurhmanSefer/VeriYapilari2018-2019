2ceac503f7a616e5db6fe6b8124e9c4646fbff25
AbdurhmanSefer <46073688+AbdurhmanSefer@users.noreply.github.com>
2018-12-22 21:31:26 +0300

    Add files via upload

---
+++ Hashing-Examble-master/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tower
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+            Hashtable symbols = new Hashtable();
+
+            // symbols.Add(Key,Vlaue);
+            symbols.Add(7, 100000);
+              symbols.Add("isim", "Ali can");
+              symbols["cinsiyet"] = "Male";
+              symbols.Add("yas", 43);
+              symbols.Add("bolum", "Bilgisayar Müh.");
+            symbols.Remove(7);
+            object  value = symbols[7];
+          
+            Console.WriteLine("Turkçesi: "+value.ToString());
+            Console.WriteLine();
+
+
+           
+        }
+    }
+}
